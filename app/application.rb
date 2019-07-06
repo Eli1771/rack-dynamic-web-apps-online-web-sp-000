33ec77ff54cabd012368076e28220fe417641eab
elijahmm17 <elijahmm17@gmail.com>
2019-07-06 18:54:40 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,8 +8,11 @@ class Application
     num_3 = Kernel.rand(1..20)
     
     if num_1 == num_2 && num_2 == num_3 
-      
-    end
+      resp.write "You Win"
+    else
+      resp.write "You Lose"
+    end 
+    
     resp.finish
   end
 
